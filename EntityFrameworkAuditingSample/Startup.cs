383411785242329f690a5892e0b830f64b853b25
Gordon Beeming <gordon@beeming.co.za>
2017-01-05 11:51:12 +0200

    Add project files.

---
+++ EntityFrameworkAuditingSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EntityFrameworkAuditingSample.Startup))]
+namespace EntityFrameworkAuditingSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
